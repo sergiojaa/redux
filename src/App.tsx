9ef842a9b01a0bcdc449d9f5085adf32506c22dc
Sergi kaliashvili <kaliashvilisergi@gmail.com>
2025-01-08 16:55:37 +0400

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      redux 
+    </div>
+  )
+}
+
+export default App
---
+++ src/store.ts
@@ -0,0 +1,9 @@
+const initialState = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: '',
+}
+//state rom ar gvkondes initial states aigebs sawkisad tavis dazgvevis miznit
+function reducer(state = initialState ,action){
+
+}
