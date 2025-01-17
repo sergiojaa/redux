28e7909f918d7c906aa01d5d06c9c521bdb29dc6
seriojaa <kaliashvilisergi@gmail.com>
2025-01-17 18:52:34 +0400

    complete redux

--- src/features/accounts/AccountOperations.tsx
+++ src/features/accounts/AccountOperations.tsx
@@ -1,49 +1,61 @@
-import { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { AppDispatch, RootState } from "../../store"
-import { deposit, payLoan, requestLoan, withdraw } from "./AccountSlice"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { deposit, payLoan, requestLoan, withdraw } from "./accountSlice";
 
 export default function AccountOperations() {
-  const [depositAmount, setDepositAmount] = useState<string | number>('')
-  const [withdrawAmount, setWithdrawAmount] = useState<string | number>('')
-  const [loanAMount, setLoanAmount] = useState<string | number>('')
-  const [loaPurpose, setLoanPurpose] = useState('')
-  const [currency, setCurrency] = useState<string>('usd')
-  const account = useSelector((store: RootState) => store.account)
-  console.log(account)
-  const dispatch = useDispatch<AppDispatch>()
-  const handleDeposit = () => {
-    if (!depositAmount) return
-    dispatch(deposit(+depositAmount))
-    setDepositAmount('')
-  }
-  const handlePayLoan = () => {
-    dispatch(payLoan())
-  }
-  const handleRequestLoan = () => {
-    if (!loanAMount) return;
+  const [depositAmount, setDepositAmount] = useState<string | number>("");
+  const [withdrawAmount, setWithdrawAmount] = useState<string | number>("");
+  const [loanAmount, setLoanAmount] = useState<string | number>("");
+  const [loanPurpose, setLoanPurpose] = useState<string>("");
+  const [currency, setCurrency] = useState<string>("USD");
 
-    dispatch(requestLoan(+loanAMount, loaPurpose))
-    setLoanAmount('')
-    setLoanPurpose('')
-  }
+  const { isLoading } = useSelector((store: RootState) => store.account);
+  console.log(isLoading);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleDeposit = () => {
+    if (!depositAmount) return;
+
+    dispatch(deposit(+depositAmount, currency));
+    setDepositAmount("");
+  };
+
   const handleWithdraw = () => {
     if (!withdrawAmount) return;
-    dispatch(withdraw(+withdrawAmount))
-    setWithdrawAmount('')
-  }
+
+    dispatch(withdraw(+withdrawAmount));
+    setWithdrawAmount("");
+  };
+
+  const handleRequestLoan = () => {
+    if (!loanAmount) return;
+
+    dispatch(requestLoan(+loanAmount, loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  };
+
+  const handlepayLoan = () => {
+    dispatch(payLoan());
+  };
+
   return (
     <div>
-      <h2>your account operations</h2>
+      <h2>Your account operations</h2>
+
       <div>
         <div>
           <div>
-            <label htmlFor="">Deposit</label>
+            <label>Deposit</label>
             <input
+              type="number"
               value={depositAmount}
               onChange={(e) => setDepositAmount(+e.target.value)}
-              type="number" />
+            />
           </div>
+
           <select
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
@@ -51,30 +63,41 @@ export default function AccountOperations() {
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
             <option value="GBP">GBP</option>
-
           </select>
-          <button onClick={handleDeposit}>Deposit</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : "Deposit"}
+          </button>
         </div>
+
         <div>
-          <div>
-            <label htmlFor="">withdraw</label>
-            <input onChange={(e) => setWithdrawAmount(+e.target.value)} value={withdrawAmount} type="number" />
-            <button onClick={handleWithdraw}>withdraw</button>
-          </div>
-          <div>
-            <label htmlFor="">Request Loan</label>
-            <input onChange={(e) => setLoanAmount(+e.target.value)} value={loanAMount} type="number" />
-            <input onChange={(e) => setLoanPurpose(e.target.value)} value={loaPurpose} type="text" />
-            <button onClick={handleRequestLoan}>Request Loan</button>
+          <label>Withdraw</label>
+          <input
+            type="number"
+            value={withdrawAmount}
+            onChange={(e) => setWithdrawAmount(+e.target.value)}
+          />
+          <button onClick={handleWithdraw}>Withdraw</button>
+        </div>
 
+        <div>
+          <label>Request Loan</label>
+          <input
+            type="number"
+            value={loanAmount}
+            onChange={(e) => setLoanAmount(+e.target.value)}
+          />
+          <input
+            type="text"
+            value={loanPurpose}
+            onChange={(e) => setLoanPurpose(e.target.value)}
+          />
+          <button onClick={handleRequestLoan}>Request Loan</button>
+        </div>
 
-          </div>
-          <div>
-            <button onClick={handlePayLoan}>pay Loan</button>
-
-          </div>
+        <div>
+          <button onClick={handlepayLoan}>Pay Loan</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,17 @@
-import { createStore , combineReducers} from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import accountReducer from "./features/accounts/AccountSlice";
 import customerReducer from "./features/customers/CustomersSlice";
+import { thunk } from "redux-thunk";
 import { deposit } from "./features/accounts/AccountSlice";
 const rootReducer = combineReducers({
     account: accountReducer,
     customer: customerReducer
 })
+
 export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
-const store =  createStore(rootReducer)
+const store = createStore(rootReducer,
+    applyMiddleware(thunk)
+)
 
 export default store
